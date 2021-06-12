e496a0b89d2328f434eccd43d082f5e01dd800bd
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-12 17:54:47 +0200

    Add files via upload

---
+++ ponavljanje1/ponavljanje1/KlasaG.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje1
+{
+    class KlasaG
+    {
+        public void Plus10(int zbroj = 10)
+        {
+
+           zbroj = zbroj + 10;
+
+            Console.WriteLine("Zbroj je: " + zbroj);
+        }
+
+        public void f50(int broj)
+        {
+            broj = 50;
+
+            Console.WriteLine("Broj je: " + broj);
+        }
+    }
+}
---
+++ ponavljanje1/ponavljanje1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaG objekt = new KlasaG();
+
+            int zbroj = 10, broj = 10;
+
+            objekt.Plus10(zbroj);
+            objekt.f50(zbroj);
+
+
+            Console.ReadKey();
+        }
+    }
+}
